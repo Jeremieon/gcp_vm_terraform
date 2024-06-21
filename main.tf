61675f77d77ec7b28e0ab17d1c20903f3470828c
Jeremieon <jerry_slade@outlook.com>
2024-06-21 22:44:50 +0200

    upgraded app version

--- main.tf
+++ main.tf
@@ -91,7 +91,7 @@ resource "google_compute_instance" "vm_instance" {
                 sudo apt-get install -y docker.io
                 sudo systemctl start docker
                 sudo docker pull jeremy9k/tanto
-                sudo docker run -d -p 80:8000 jeremy9k/tanto
+                sudo docker run -d -p 80:8000 jeremy9k/tanto:v01
                 EOT
   }
 }
